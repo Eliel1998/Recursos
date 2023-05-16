283f786634de7647377d2abace22cc3ef75f18b0
Eliel Oliver <eliel.viana@grupomateus.com>
2023-05-16 19:40:52 -0300

    sms

--- app/src/main/java/com/example/recursos/MainActivity.java
+++ app/src/main/java/com/example/recursos/MainActivity.java
@@ -3,33 +3,43 @@ package com.example.recursos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonGmail;
+    private Button buttonSMS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonGmail = findViewById(R.id.buttonGmail);
+        buttonSMS = findViewById(R.id.buttonSms);
     }//end onCreate
 
-    public void clicar(View view){
+    public void clicar(View view) {
         Intent intent = null;
-        if(view.getId() == R.id.buttonGmail){
-            String email = "apag.guimaraes@gmail.com";
-            String subject = "Meu titulo";
-            String text = "Meu texto";
-            intent = new Intent(Intent.ACTION_SEND);
-            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
-            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-            intent.putExtra(Intent.EXTRA_TEXT, text);
-            intent.setType("message/rfc822");
-            intent.setPackage("com.google.android.gm");
-            startActivity(intent);
+        if (view.getId() == R.id.buttonSms) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                String defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(this);
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello World!");
+                if (defaultSmsPackageName != null) {
+                    intent.setPackage(defaultSmsPackageName);
+                    startActivity(intent);
+                }
+            } else {
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello World! 2");
+                intent.setType("vnd.android-dir/mms-sms");
+                startActivity(intent);
+            }
         }
     }
 }
