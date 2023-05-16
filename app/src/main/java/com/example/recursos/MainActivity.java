011711aaf2bd692aa90093aaaf36719336a1c05c
Eliel Oliver <eliel.viana@grupomateus.com>
2023-05-16 20:12:05 -0300

    camera

--- app/src/main/java/com/example/recursos/MainActivity.java
+++ app/src/main/java/com/example/recursos/MainActivity.java
@@ -3,33 +3,42 @@ package com.example.recursos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonWhats;
+    private static final int REQUEST_PHOTO = 1;
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonWhats = findViewById(R.id.buttonWhatsapp);
+        imageView = findViewById(R.id.imageViewCamera);
+        button = findViewById(R.id.buttonCamera);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_PHOTO);
+            }
+        });
     }//end onCreate
 
-    public void clicar(View view) {
-        Intent intent = null;
-        if (view.getId() == R.id.buttonWhatsapp) {
-            String msg = "Olá, tudo bem?";
-            intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.putExtra(Intent.EXTRA_TEXT, msg);
-            intent.setType("text/plain");
-            intent.setPackage("com.whatsapp");
-            startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PHOTO) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(image);
         }
     }
+
 }
